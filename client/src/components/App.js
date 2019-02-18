12938bd854c6f6d4cf2376f25b3a1c3ec8ba53ec
Giri Vakkalanka <gvakka@gmail.com>
2019-02-18 18:05:59 -0500

    added item form component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import Landing from './Landing';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import SurveyNew from './surveys/SurveyNew';
-
+import ItemForm from './items/ItemForm';
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
@@ -20,6 +20,7 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
             <Route path="/surveys/new" component={SurveyNew} />
+            <Route path="/items/new" component={ItemForm}/>
           </div>
         </BrowserRouter>
       </div>
