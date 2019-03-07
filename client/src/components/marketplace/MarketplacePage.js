27b81351b9bbf775255f89427c01322854c72b51
Giri Vakkalanka <gvakka@gmail.com>
2019-03-07 08:34:14 -0500

    added marketplace component

--- client/src/components/marketplace/MarketplacePage.js
+++ client/src/components/marketplace/MarketplacePage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../../actions/index';
 
 class MarketplacePage extends Component {
+  componentDidMount() {
+    console.log('test component');
+  }
   render(){
     return <div>Marketplace Page</div>;
   }
 }
 
-export default MarketplacePage;
+function mapStateToProps(state){
+  return null;
+}
+
+export default connect(mapStateToProps, actions)(MarketplacePage);
