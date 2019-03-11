49ea924ecf6af63734eece8985ea022f3fb56ac1
Giri Vakkalanka <gvakka@gmail.com>
2019-03-11 15:57:31 -0400

    fixed stripe error

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Payments from './Payments';
+//import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -18,7 +18,7 @@ class Header extends Component {
       default:
         return [
           <li key="1">
-            <Payments />
+            <div>Payments</div>
           </li>,
           <li key="3" style={{ margin: '0 10px' }}>
             Credits: {this.props.auth.credits}
--- client/src/components/marketplace/MarketplacePage.js
+++ client/src/components/marketplace/MarketplacePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import * as actions from '../../actions/index';
 import ItemCard from '../items/ItemCard';
 import _ from 'lodash';
