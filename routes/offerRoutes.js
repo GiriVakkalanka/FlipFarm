f481bea309d03a176e966c368f9a2928e44caa62
Giri Vakkalanka <gvakka@gmail.com>
2019-03-12 17:49:07 -0400

    configured offer back end submission

--- routes/offerRoutes.js
+++ routes/offerRoutes.js
@@ -4,12 +4,23 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Item = mongoose.model('Item');
 const User = mongoose.model('users');
+const Offer = mongoose.model('Offer');
 
 module.exports = app => {
   app.post('/api/submit_offer', requireLogin, async (req, res) => {
     console.log(req.body);
-    //const offer = new Offer({ });
-    res.send('route works');
+    const { itemWanted, itemOffered } = req.body;
+    const wantedItem = await Item.findOne({_id: itemWanted});
+    offerTo = wantedItem._user
+    const offer = new Offer({
+      itemWanted,
+      itemOffered,
+      offerFrom: req.user.id,
+      offerTo: wantedItem._user,
+      offerDate: Date.now()
+    });
+    console.log(offer)
+    res.send(offer) ;
   });
 }
 
